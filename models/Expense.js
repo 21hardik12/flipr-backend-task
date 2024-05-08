75b7a4832f2c9fe63f2e1c9d812b1788d6e25db6
21hardik12 <21hardik12@gmail.com>
2024-05-08 16:29:06 +0530

    Add category, Expense and User models

---
+++ models/Expense.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Expense = mongoose.model.Expense || mongoose.model('Expense', expenseSchema);
+
+module.exports = Expense;
